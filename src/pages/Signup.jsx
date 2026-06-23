4ef38443b3b5162762ef7249628dd458c95f4906
kartik9433 <kartiksingh8923@gmail.com>
2026-06-23 13:44:41 +0530

    auth to Auth

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { signup } from "../api/authapi";
+import { signup } from "../api/Authapi";
 import { Activity } from "lucide-react";
 
 export default function Signup() {
